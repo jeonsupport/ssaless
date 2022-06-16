43b534803ec261888c3e2296aacc0b43003bd6ad
Jiwon Jennie Jeon <jeonz1282@gmail.com>
2022-06-16 16:47:26 +0900

    width 변경

--- login.php
+++ login.php
@@ -17,7 +17,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
         <style>
             body{font-family:'Noto Sans KR','Open Sans',sans-serif;height:100%;margin:0;padding:0}
-            #login{width:450px;background:#fff;padding:120px 50px 150px;position:relative;margin:0 auto}
+            #login{width:350px;background:#fff;padding:120px 50px 150px;position:relative;margin:0 auto}
             #login h1{margin:30px 0 60px;color:#333;font-size:22px;line-height:1.2}
             #login .inputBox{border-bottom:2px solid #adadad;position:relative;margin:30px 0}
             #login .inputBox input{font-size:15px;color:#333;border:none;width:100%;outline:none;background:none;height:40px;text-indent:10px}
